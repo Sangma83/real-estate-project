f0f3c68b38115396d07480dc0a8f02d51d5cdcda
Sangma83 <poushalisangma13@gmail.com>
2024-04-16 16:09:26 +0600

    Home page

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8jKabgoacrFRrEYvLG_VBUXeVcTmUgos",
+  authDomain: "residential-real-estate-1aef9.firebaseapp.com",
+  projectId: "residential-real-estate-1aef9",
+  storageBucket: "residential-real-estate-1aef9.appspot.com",
+  messagingSenderId: "890399646651",
+  appId: "1:890399646651:web:ed6e21f35eef62d32cda9e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/routes/Route.jsx
@@ -0,0 +1,20 @@
+
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Root from "../layouts/Root";
+
+
+const route = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root></Root>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+]);
+
+export default route;
